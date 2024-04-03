602c253df7ad6163c5dbabc59d85ddb10d94f1a7
Gaurav kumar <gaurav@kreyaconsulting.com>
2024-04-03 23:38:21 +0530

    comments added for feature branch

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("i am first");
 console.log("i am second");
+
+
+//add feature function comments for feature branch 
+function newFeatureAdd(){
+    console.log('this is feature branch cod');
+}
